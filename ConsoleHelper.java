525c3d79a7e8ba0fea85848bd4c78cef95a289fa
wib <vitalij.boshko@gmail.com>
2021-05-16 18:30:16 +0300

    fixed class ConsoleHelper

--- ConsoleHelper.java
+++ ConsoleHelper.java
@@ -5,20 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConsoleHelper {
-    public static void writeMessage(String message){
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
         System.out.println(message);
     }
 
     public static String readString() throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        writeMessage("Введите команду...");
-        String stringMes = bufferedReader.readLine();
-        return stringMes;
+        String text = bis.readLine();
+        return text;
     }
 
-    public static int readInt()throws IOException{
-        writeMessage("Введите число...");
-        Integer intMes = Integer.parseInt(readString());
-        return intMes;
+    public static int readInt() throws IOException {
+        String text = readString();
+        return Integer.parseInt(text.trim());
     }
 }
