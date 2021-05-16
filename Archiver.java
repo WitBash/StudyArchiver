6bdc32d46f8e1eee5f2c1e9b06b2c6f7ba24556c
wib <vitalij.boshko@gmail.com>
2021-05-16 19:11:37 +0300

    added command classes

--- Archiver.java
+++ Archiver.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
@@ -11,5 +13,8 @@ public class Archiver {
         ZipFileManager zipFileManager = new ZipFileManager(Paths.get(pathTo));
         String pathFrom  = bufferedReader.readLine();
         zipFileManager.createZip(Paths.get(pathFrom));
+
+        ExitCommand exitCommand = new ExitCommand();
+        exitCommand.execute();
     }
 }
