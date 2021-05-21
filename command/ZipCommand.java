a2507a9ef603508b4b442c82d83d51156da60a46
wib <vitalij.boshko@gmail.com>
2021-05-21 21:45:24 +0300

    added functionality creating archive

--- command/ZipCommand.java
+++ command/ZipCommand.java
@@ -1,5 +1,18 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command{
 
+    public ZipFileManager getZipFileManager() throws Exception{
+        ConsoleHelper.writeMessage("Введите полный путь файла архива");
+        String stringPath = ConsoleHelper.readString();
+        Path filePath = Paths.get(stringPath);
+        ZipFileManager zipFileManager = new ZipFileManager(filePath);
+        return zipFileManager;
+    }
 }
