50f78e21ec3432f4acc06a3c4e04ec79ccfcf29d
wib <vitalij.boshko@gmail.com>
2021-05-21 23:15:15 +0300

    added  files properties description

--- command/ZipCommand.java
+++ command/ZipCommand.java
@@ -6,13 +6,11 @@ import com.javarush.task.task31.task3110.ZipFileManager;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public abstract class ZipCommand implements Command{
+public abstract class ZipCommand implements Command {
 
-    public ZipFileManager getZipFileManager() throws Exception{
-        ConsoleHelper.writeMessage("Введите полный путь файла архива");
-        String stringPath = ConsoleHelper.readString();
-        Path filePath = Paths.get(stringPath);
-        ZipFileManager zipFileManager = new ZipFileManager(filePath);
-        return zipFileManager;
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
 }
